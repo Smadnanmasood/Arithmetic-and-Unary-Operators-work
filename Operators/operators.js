56ddb9588dd39544764bc1a6a06356197418d78a
Syed Muhammad Adnan Masood <160840496+Smadnanmasood@users.noreply.github.com>
2024-03-19 00:35:02 +0500

    Arithmetic and Unary Operators
    Hi Fox,
    I have done some work regarding Arithmetic operators and Unary operators in this repository. I used command prompt in VSC to print the code.

---
+++ Operators/operators.js
@@ -0,0 +1,39 @@
+//Arthimatic operators
+// +, -, *, /, % (Modulus), ** (exponentiation)
+// let a=8
+// let b=3
+// console.log("a=", a, "& b=", b);
+// console.log("a+b =", a+b); //11
+// console.log("a-b =", a-b); //5
+// console.log("a*b =", a*b); //24
+// console.log("a/b =", a/b); //2.6666666665
+// console.log("a%b =", a%b); //2
+//console.log("a**b =", a**b); //512
+//Unary Operators
+// ++ (increment), -- (decrement)
+// a++ (post increment), ++a (pre increment), a-- (post decrement), --a (pre decrement)
+var a = 8;
+var b = 3;
+console.log("a=", a, "& b=", b);
+//console.log("a+1 =", a+1); //9
+//console.log("++a =", ++a); //9
+// a=a+1
+// console.log("a=", a) //9
+// a++;
+// console.log("a=", a) //9
+// a=a-1
+// console.log("a=", a) //7
+// a--;
+// console.log("a=", a) //7
+// a++ (post increment), ++a (pre increment), a-- (post decrement), --a (pre decrement)
+// console.log("a++=", a++); //8
+// console.log("a++=", a); //9
+// console.log("++a=", ++a); //9
+// console.log("a++=", a); //9
+// console.log("a--=", a--); //8
+// console.log("a=", a); //7
+// console.log("--a=", --a); //7
+// console.log("a=", a); //7
+console.log("--a=", --a); //7
+console.log("a=", a); //7
+console.log("a=", a); //7
